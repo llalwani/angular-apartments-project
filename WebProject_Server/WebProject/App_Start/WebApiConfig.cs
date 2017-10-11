de7ecb2fc393296498b109882ff5627b90e49f05
saif <saif.as7777@gmail.com>
2017-10-11 14:21:07 +0300

    Integrate login with server

--- WebProject_Server/WebProject/App_Start/WebApiConfig.cs
+++ WebProject_Server/WebProject/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace WebProject
 {
@@ -12,6 +13,8 @@ namespace WebProject
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
@@ -20,7 +23,7 @@ namespace WebProject
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.EnableCors();
+
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
--- WebProject_Server/WebProject/Controllers/AuthenticationController.cs
+++ WebProject_Server/WebProject/Controllers/AuthenticationController.cs
@@ -10,12 +10,23 @@ using System.Web.Http.Cors;
 namespace WebProject.Controllers
 {
   //  [EnableCorsAttribute("http://localhost:50743", "*", "*")
-    [EnableCors(origins: "*", headers: "*", methods: "*")]
+   // [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class AuthenticationController : ApiController
     {
         //Register
         // POST api/values
 
+        [HttpGet, Route("api/authentication")]
+        public IHttpActionResult Get(string username, string password)
+        {
+            var s = new ApartmentService();
+
+            if (!s.CheckIfValidLogin(username, password))
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
         //http://localhost:64014a/pi/authentication
         [HttpPost, Route("api/authentication")]
         public IHttpActionResult Post([FromBody]User user)
@@ -41,17 +52,7 @@ namespace WebProject.Controllers
 
 
         //http://localhost:64014/api/authentication?Username=Mia&Password=dsad
-        [HttpGet, Route("api/authentication")]
-        public IHttpActionResult Get(string username, string password)
-        {
-            var s = new ApartmentService();
 
-            if (!s.CheckIfValidLogin(username, password))
-            {
-                return NotFound();
-            }
-            return Ok();
-        }
 
         [HttpPut, Route("api/authentication")]
         public IHttpActionResult Put([FromBody]User user, string newPassword)
